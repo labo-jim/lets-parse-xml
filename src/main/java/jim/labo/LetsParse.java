ac75336fa48f67d367ba80b289e9ba615c61480c
Jim ETEVENARD <j.etevenard-ext@lefebvre-sarrut.eu>
2018-06-25 14:43:42 +0200

    initial commit :)

---
+++ src/main/java/jim/labo/LetsParse.java
@@ -0,0 +1,23 @@
+package jim.labo;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LetsParse {
+
+	public static void main(String[] args) {
+		// Let's Parse XML !!
+		
+		InputStream exemple = fichierExemple();
+		
+		
+		// Dom, Sax, XPath, JAXB...
+
+	}
+
+	private static InputStream fichierExemple() {
+		final String ex = "toto-clients.xml";
+		return LetsParse.class.getClassLoader().getResourceAsStream(ex);
+	}
+
+}
